afc63cc933d766df5d1d70fc3b336436ecbd7a4e
NaviSiuTu <isantisteban@unal.edu.co>
2025-05-04 19:27:27 -0500

    Realización de Ejercicios 18 y 19

---
+++ DeEurosADolares.py
@@ -0,0 +1,7 @@
+def convertidorMoneda(x): #Definición de función para convertir una moneda
+    TasaConversión = x * 1.13 #Tasa de conversión de la moneda actualmente
+    print ("---- Conversión ----" + f"\n{x}€ son ${TasaConversión} dolares") #Impresión de la conversión
+
+x = float(input("---- Datos ----\nDisponga sus euros aquí: ")) #Interacción del usuario con la consola 
+convertidorMoneda(x)
+  
